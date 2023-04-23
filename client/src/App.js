241549107902675d7fb1da9d3ef7e7e3b2f13e66
TBewice010 <64942741+benny12312311@users.noreply.github.com>
2023-04-23 23:57:46 +0800

    Add files via upload

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+    return (
+    <div className="wrapper">
+      <div className="content-wrapper">
+       <h1> Hello world! </h1>
+      </div>
+    </div>
+  )
+}
+
+export default App;
